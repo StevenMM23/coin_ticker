bef9a3e5b19608dcd14dd0bf55b4f6a4de1dd07a
Steven Martinez <esteven123123@outlook.com>
2023-09-25 00:40:06 -0400

    Finish Coin Ticker

--- lib/coin_data.dart
+++ lib/coin_data.dart
@@ -1,3 +1,6 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
 const List<String> currenciesList = [
   'AUD',
   'BRL',
@@ -28,4 +31,27 @@ const List<String> cryptoList = [
   'LTC',
 ];
 
-class CoinData {}
+class CoinData {
+  static const apiKey = 'AA824EC4-01AE-42CC-ACE8-55C5C4C88F29';
+
+  Future<Map<String, double>> getExchangeRates(String currency) async {
+    Map<String, double> exchangeRates = {};
+
+    for (String crypto in cryptoList) {
+      final response = await http.get(
+        Uri.parse('https://rest.coinapi.io/v1/exchangerate/$crypto/$currency'),
+        headers: {'X-CoinAPI-Key': apiKey},
+      );
+
+      if (response.statusCode == 200) {
+        final data = json.decode(response.body);
+        double rate = data['rate'];
+        exchangeRates[crypto] = rate;
+      } else {
+        throw Exception('Error al obtener los datos de la API');
+      }
+    }
+
+    return exchangeRates;
+  }
+}
--- lib/crypto_card.dart
+++ lib/crypto_card.dart
@@ -1,54 +1,125 @@
-
-
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-
 import 'coin_data.dart';
 
 class CryptoCardScreen extends StatefulWidget {
+  const CryptoCardScreen({super.key});
+
   @override
-  _CryptoCardScreenState createState() => _CryptoCardScreenState();
+  CryptoCardScreenState createState() => CryptoCardScreenState();
 }
 
-class _CryptoCardScreenState extends State<CryptoCardScreen> {
-  String selectedCurrency = 'USD'; // Valor inicial del dropdown
+class CryptoCardScreenState extends State<CryptoCardScreen> {
+  String selectedCurrency = 'USD';
+  Map<String, double> exchangeRates = {};
+
+  @override
+  void initState() {
+    super.initState();
+    updateExchangeRates();
+  }
+
+  void updateExchangeRates() async {
+    try {
+      final data = await CoinData().getExchangeRates(selectedCurrency);
+      setState(() {
+        exchangeRates = data;
+      });
+    } catch (e) {
+      print('Error: $e');
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Crypto Cards'),
+        title: const Text('🤑 Coin Ticker'),
+        backgroundColor: Colors.blue,
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            DropdownButton<String>(
-              value: selectedCurrency,
-              items: currenciesList.map((String currency) {
-                return DropdownMenuItem<String>(
-                  value: currency,
-                  child: Text(currency),
-                );
-              }).toList(),
-              onChanged: (newValue) {
-                setState(() {
-                  selectedCurrency = newValue!;
-                });
-              },
-            ),
-            SizedBox(height: 20),
-            for (String crypto in cryptoList)
-              Card(
-                color: Colors.blue,
-                child: ListTile(
-                  title: Text('1 $crypto: $selectedCurrency'),
-                  subtitle: Text('Crypto card content goes here'),
-                ),
+      body: Column(
+        children: [
+          const SizedBox(height: 40),
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20.0),
+              child: ListView(
+                children: [
+                  for (String crypto in cryptoList)
+                    Padding(
+                      padding: const EdgeInsets.only(bottom: 16.0),
+                      child: CryptoCard(
+                        crypto: crypto,
+                        selectedCurrency: selectedCurrency,
+                        rate: exchangeRates[crypto] ?? 0,
+                      ),
+                    ),
+                ],
               ),
-          ],
-        ),
+            ),
+          ),
+          Container(
+            color: Colors.blue,
+            width: double.infinity,
+            padding: const EdgeInsets.symmetric(vertical: 100),
+            child: Center(
+              child: DropdownButton<String>(
+                value: selectedCurrency,
+                items: currenciesList.map((String currency) {
+                  return DropdownMenuItem<String>(
+                    value: currency,
+                    child: Text(
+                      currency,
+                      style: const TextStyle(
+                        color: Colors.black,
+                        fontSize: 18.0,
+                      ),
+                    ),
+                  );
+                }).toList(),
+                onChanged: (newValue) {
+                  setState(() {
+                    selectedCurrency = newValue!;
+                    updateExchangeRates();
+                  });
+                },
+              ),
+            ),
+          ),
+        ],
       ),
+    );
+  }
+}
+
+class CryptoCard extends StatelessWidget {
+  final String crypto;
+  final String selectedCurrency;
+  final double rate;
+
+  const CryptoCard({
+    super.key,
+    required this.crypto,
+    required this.selectedCurrency,
+    required this.rate,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      elevation: 10,
+      margin: const EdgeInsets.symmetric(horizontal: 8.0, vertical: 4.0),
+      color: Colors.blue,
+      child: ListTile(
+        title: Text(
+          '1 $crypto = ${rate.toInt()} $selectedCurrency',
+          style: const TextStyle(
+            color: Colors.white,
+            fontSize: 16.0,
+            fontWeight: FontWeight.bold,
+          ),
+          textAlign: TextAlign.center,
+        ),
+      ), //
     );
   }
 }
